3d0fbe7a9446b3610f92e870a4a5b2e9f0d4180a
Yian Jin <yianj1@uci.edu>
2023-11-25 19:11:13 -0800

    将StudentAI.py改为使用蒙特卡洛树搜索版。 修改了calculate_win_rates.py使其能在使用python 3.5.2的情况下也能运行。 P.S. 目前Poor_AI只能在python/3.5.2下运行，因此测试输入的顺序是： $ module load python/3.5.2 $ python3 calculate_win_rates.py

--- StudentAI.py
+++ StudentAI.py
@@ -1,3 +1,149 @@
+import math
+import random
+from copy import deepcopy
+from random import randint
+from BoardClasses import Move, Board, Checker
+
+
+# The following part should be completed by students.
+# Students can modify anything except the class name and exisiting functions and varibles.
+
+
+class MCTS_Node:
+    def __init__(self, move = None, parent = None, board = None, player_color = None) -> None:
+        self.move = move
+        self.parent = parent
+        self.board = deepcopy(board)
+        self.children = []
+        self.val = 0.0
+        self.visits = 0.0
+        self.player_color = player_color
+        self.untried_moves = self.board.get_all_possible_moves(player_color)
+
+    def UCT_select_child(self):
+        c = math.sqrt(2)
+        best_child = None
+        best_score = -float('inf')
+        for child in self.children:
+            if child.visits > 0:
+                win_ratio = child.val / child.visits
+                uct_score = win_ratio + c * math.sqrt(math.log(self.visits) / child.visits)
+            else:
+                uct_score = float('inf')
+            if uct_score > best_score:
+                best_score = uct_score
+                best_child = child
+        return best_child
+
+    def add_child(self, move, board):
+        child_board = deepcopy(board)
+        child_board.make_move(move, self.player_color)
+        opponent_color = 1 if self.player_color == 2 else 2
+        child_node = MCTS_Node(move = move, parent = self, board = child_board,
+                               player_color = opponent_color)
+        self.children.append(child_node)
+        for moveset in self.untried_moves:
+            if move in moveset:
+                moveset.remove(move)
+                break
+        return child_node
+
+    def update(self, result):
+        self.visits += 1.0
+        if result == self.player_color:
+            self.val += 1.0
+
+
+def MCTS(root: MCTS_Node, current_color, iterations) -> None:
+    opponent = {1: 2, 2: 1}
+    for _ in range(iterations):
+        node = root
+
+        # 寻找有未尝试走法的节点，如果没有，则使用UCT选择子节点
+        while node.children and not any(moves for moves in node.untried_moves if moves):
+            node = node.UCT_select_child()
+
+        # 从未尝试的走法中选择一个走法进行扩展
+        untried_moves = [move for sublist in node.untried_moves for move in sublist if sublist]
+        if untried_moves:
+            move = random.choice(untried_moves)
+            node = node.add_child(move, node.board)
+
+        # 模拟随机对局并更新节点
+        simulation_result = simulate_random_games(node.board, node.player_color)
+        while node is not None:
+            node.update(simulation_result)
+            node = node.parent
+
+
+# 速度太慢 弃用 之后可以参考
+# def heuristic_move_score(board, move, player_color):
+#     score = 0.0
+#     for pos in move.seq:
+#         checker = board.board[pos[0]][pos[1]]
+#         if checker.color == player_color:
+#             if checker.is_king:
+#                 score += 5  # 王棋的权重
+#             else:
+#                 score += 1  # 普通棋子的权重
+#             # if board.is_center_square(pos):
+#             #     score += 2  # 占据中心的权重
+#     return score
+
+def simulate_random_games(board, current_color):
+    board = deepcopy(board)
+    opponent = {1: 2, 2: 1}
+
+    while True:
+        possible_moves = board.get_all_possible_moves(current_color)
+
+        # 移除空的移动列表
+        possible_moves = [m for m in possible_moves if m]
+
+        # 一但一方获胜则结束循环
+        if board.is_win(current_color) == current_color:
+            return current_color
+        if board.is_win(opponent[current_color]) == opponent[current_color]:
+            return opponent[current_color]
+
+        # 应用启发式规则 - 优先移动能吃掉其他棋子的棋子
+        capture_moves = [m for sublist in possible_moves for m in sublist if len(m.seq) > 2]
+        if capture_moves:
+            move = random.choice(capture_moves)
+        else:
+            move = random.choice(random.choice(possible_moves))
+
+        board.make_move(move, current_color)
+
+        current_color = opponent[current_color]
+
+
+class StudentAI:
+    def __init__(self, col, row, p) -> None:
+        self.col = col
+        self.row = row
+        self.p = p
+        self.board = Board(col, row, p)
+        self.board.initialize_game()
+        self.color = 2
+        self.opponent = {1: 2, 2: 1}
+
+    def get_move(self, move: Move):
+        if len(move) != 0:
+            self.board.make_move(move, self.opponent[self.color])
+        else:
+            self.color = 1
+        root = MCTS_Node(board = self.board, player_color = self.color)
+        MCTS(root, self.color, iterations = 10)
+        best_move = max(root.children, key = lambda c: c.val / c.visits).move
+        self.board.make_move(best_move, self.color)
+        return best_move
+
+
+
+
+# minimax 版
+"""
 from random import randint
 from BoardClasses import Move
 from BoardClasses import Board
@@ -117,3 +263,4 @@ class StudentAI():
         best_move = self.find_best_move(self.board, 3)  # example, depth set to 3, change later
         self.board.make_move(best_move, self.color)
         return best_move
+"""
--- calculate_win_rates.py
+++ calculate_win_rates.py
@@ -1,6 +1,109 @@
 import subprocess
 
 # pip install tqdm
+# from tqdm import tqdm
+
+import time
+
+
+def run_game():
+    command = [
+        "python",
+        "main.py",
+        "7",
+        "7",
+        "2",
+        "l",
+        "main.py",
+        "Sample_AIs/Random_AI/main.py",
+        # "Sample_AIs/Poor_AI/main.py",
+    ]
+
+    # python 3.5.2不支持
+    """
+    result = subprocess.run(command, capture_output=True, text=True)
+    return result.stdout
+    """
+    result = subprocess.run(command, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
+    stdout_text = result.stdout.decode('utf-8')
+    return stdout_text
+
+
+def calculate_win_rates(rounds = 100):
+    wins = {"player 1": 0, "player 2": 0, "ties": 0}
+
+    # module load python/3.5.2后无法使用，推测是tqdm并没有被下载到python 3.5.2中
+    """
+    for i in tqdm(range(rounds), desc="Running games"):
+        output = run_game()
+        if "player 1 wins" in output:
+            wins["player 1"] += 1
+        elif "player 2 wins" in output:
+            wins["player 2"] += 1
+        elif "Tie" in output:
+            wins["ties"] += 1
+    """
+
+    for i in range(rounds):
+        output = run_game()
+        if "player 1 wins" in output:
+            wins["player 1"] += 1
+        elif "player 2 wins" in output:
+            wins["player 2"] += 1
+        elif "Tie" in output:
+            wins["ties"] += 1
+
+        # 手动显示进度
+        progress = (i + 1) / rounds * 100
+        print("Progress: %.2f%%" % progress)
+
+    # 不适用于python3.5.2
+    """
+    win_rates = {
+        "player 1": f"{wins['player 1'] / rounds * 100:.2f}%",
+        "player 2": f"{wins['player 2'] / rounds * 100:.2f}%",
+        "ties": f"{wins['ties'] / rounds * 100:.2f}%",
+    }
+    """
+
+    win_rates = {
+        "player 1": "{:.2f}%".format(wins['player 1'] / rounds * 100),
+        "player 2": "{:.2f}%".format(wins['player 2'] / rounds * 100),
+        "ties": "{:.2f}%".format(wins['ties'] / rounds * 100),
+    }
+
+    # 不适用于python3.5.2
+    """
+    return f"player 1: {win_rates['player 1']}\nplayer 2: {win_rates['player 2']}\nties: {win_rates['ties']}"
+    """
+    return "player 1: {:.2f}%\nplayer 2: {:.2f}%\nties: {:.2f}%".format(
+        wins['player 1'] / rounds * 100,
+        wins['player 2'] / rounds * 100,
+        wins['ties'] / rounds * 100
+    )
+
+
+if __name__ == "__main__":
+    print("test 1\n")
+    print(calculate_win_rates(10))
+    # print("test 2\n")
+    # print(calculate_win_rates(10))
+    # print("test 3\n")
+    # print(calculate_win_rates(10))
+    # print("\n")
+    print("\n")
+    # python main.py 7 7 2 l Sample_AIs/Random_AI/main.py main.py
+    # python3 main.py 7 7 2 l Sample_AIs/Poor_AI/main.py main.py
+
+    # 以下为测试时输入顺序
+    # module load python/3.5.2
+    # python3 calculate_win_rates.py
+
+# 旧版
+"""
+import subprocess
+
+# pip install tqdm
 from tqdm import tqdm
 
 
@@ -42,3 +145,4 @@ def calculate_win_rates(rounds=100):
 
 if __name__ == "__main__":
     print(calculate_win_rates(10))
+"""
